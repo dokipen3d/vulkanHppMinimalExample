287ea930cfcc0f6e0a90c213cae8c093a151d9e3
Daniel Elliott <danielelliott3d@gmail.com>
2019-03-19 23:43:01 +1300

    fix header include

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <libshaderc/shaderc.hpp>
 #include <set>
-#include <vulkan/vulkan2.hpp>
+#include <vulkan/vulkan.hpp>
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
